49dc9e6982c23159546d730e033ddd22edccd22d
Drave <davidsebas1825@gmail.com>
2023-10-27 19:01:03 -0500

    Se hizo las paginas de los servicios y se hicieron unas correcciones generales a la pagina

---
+++ root/scripts/index.js
@@ -0,0 +1,20 @@
+const contentButtons = document.getElementById('content-buttons')
+
+const portatilButton = document.getElementById('portatil-button')
+const pcButton = document.getElementById('pc-button')
+const componentButton = document.getElementById('component-button')
+
+
+
+
+portatilButton.addEventListener('click', () =>{
+    
+})
+
+pcButton.addEventListener('click', () =>{
+    
+})
+
+componentButton.addEventListener('click', () =>{
+    
+})
