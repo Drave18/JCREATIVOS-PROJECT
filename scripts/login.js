f190d744a3e26e3975badf9f1909cba3a0d46ce0
Drave <davidsebas1825@gmail.com>
2023-11-26 02:09:09 -0500

    Algunos ajustes finales

--- scripts/login.js
+++ scripts/login.js
@@ -4,29 +4,29 @@
 //  Estos parámetros representan el ID del campo de contraseña y el
 //  ID del ícono del ojo en el formulario de inicio de sesión, respectivamente.
 const showHiddenPass = (loginPass, loginEye) =>{
-   // Se obtienen referencias a los elementos del DOM utilizando los IDs proporcionados como parámetros. 
+   // Se obtienen referencias a los elementos del DOM utilizando los IDs proporcionados como parámetros.
    // input es el campo de contraseña y iconEye es el ícono del ojo.
     const input = document.getElementById(loginPass),
           iconEye = document.getElementById(loginEye)
- 
+
     iconEye.addEventListener('click', () =>{
     //    cambiar contraseña a texto
    //  Se agrega un event listener al ícono del ojo que escucha el evento de clic.
    // Alternar entre Contraseña Visible y Oculta:
-   //Dentro del event listener, se verifica el tipo de entrada (input.type). Si la contraseña está oculta 
+   //Dentro del event listener, se verifica el tipo de entrada (input.type). Si la contraseña está oculta
    //(tipo 'password'), se cambia a texto y se actualiza el icono. Si la contraseña ya es visible (tipo 'text'),
    // se cambia nuevamente a contraseña y
    // se actualiza el icono correspondientemente.
        if(input.type === 'password'){
           input.type = 'text'
- 
+
           // cambiar icono
           iconEye.classList.add('ri-eye-line')
           iconEye.classList.remove('ri-eye-off-line')
        } else{
           // cambiar a contraseña
           input.type = 'password'
- 
+
           // cambiar icono
           iconEye.classList.remove('ri-eye-line')
           iconEye.classList.add('ri-eye-off-line')
@@ -75,6 +75,8 @@ function verifyUser(event){
           const loggedUser = { ...data, login: true };
           localStorage.setItem("currentUser", JSON.stringify(loggedUser));
           redirectTo("../index.html");
+        // si el valor de samePassword es true, con ! se convierte a false y se ejecuta la condicion
+        // pero si el valor es false , con ! se convierte a true y no se ejecuta la condicion
         } else if (!samePassword) {
          errorMessage.innerText =
          "Contraseña incorrecta";
