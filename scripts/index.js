c15fd7af838d43885d7d2bffaf286173544a6770
Drave <davidsebas1825@gmail.com>
2023-11-26 21:42:27 -0500

    Otros ajustes finales

--- scripts/index.js
+++ scripts/index.js
@@ -178,42 +178,6 @@ document.addEventListener("DOMContentLoaded", function() {
 // Obtener los datos del usuario desde el almacenamiento local
 
 
-const userInfo = document.getElementById("username");
-
-function ShowUserName(){
-    currentUser = localStorage.getItem("currentUser");
-    if (currentUser){
-        const p = document.createElement("p");
-        const user = JSON.parse(currentUser);
-        p.textContent = user.Name;
-        userInfo.appendChild(p);
-        // mostrar info de usuario
-        userInfo.style.display = "block";
-        // quitar boton de login y register
-        const login = document.getElementById("login");
-        login.style.display = "none";
-        const register = document.getElementById("register");
-        register.style.display = "none";
-
-        //boton de logout aparecer 
-        const logout = document.getElementById("logout");
-        logout.style.display = "block";
-        logout.addEventListener("click",()=>{
-            p.textContent = ""
-            userInfo.style.display = "none";
-            localStorage.removeItem("currentUser");
-            logout.style.display = "none";
-
-            login.style.display = "block";
-            register.style.display = "block";
-
-        })
-    }
-}
-
-ShowUserName()
-
-
 
 
 //HOVER PARA BOTONES
